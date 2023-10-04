7a8e89338262ec3981aea1171704dee875f0064f
Aranda <ajabiro1211@gmail.com>
2023-10-04 18:55:01 -0400

    initial commit

---
+++ lab1.py
@@ -0,0 +1,35 @@
+# 1.1 Echo String
+text = input("Enter some text: ")
+print(text)
+
+# 1.2 Adding a num to an int
+number = int(input("Enter a number: "))
+print(number + 1)
+
+# 1.3 Adding a number to a float
+number = float(input("Enter a number: "))
+print(number + 0.5)
+
+# 1.4 Adding two floats
+user_number1 = float(input("Enter a number: "))
+user_number2 = float(input("Enter another number: "))
+sum = user_number1 + user_number2
+print(f"The sum is {sum}")
+
+#1.5 Multiplying floats
+user_number1 = float(input("Enter a number: "))
+user_number2 = float(input("Enter another number: "))
+product = user_number1 * user_number2
+print(f"The product is {product}")
+
+#1.6 Dividing integers
+user_number1 = int(input("Enter a number: "))
+user_number2 = int(input("Enter another number: "))
+result = user_number1 / user_number2
+print(f"The result is {result}")
+
+#1.7 Entering booleans
+# example = input(bool("Enter a boolean: "))
+# print("You entered: {example}")
+# print("The opposite of what you entered is: ")
+
